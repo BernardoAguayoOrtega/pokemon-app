f84dfb691aa028ba4bf3d76eb783e4ac6ba243c4
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 16:46:10 -0500

    set up

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//import react
+import React from 'react'
+//import react react dom
+import ReactDom from 'react-dom'
+
+//create root const
+const root = document.getElementById('root')
+
+//render react dom
+ReactDom.render(
+	<h1>Set up!!!</h1>,
+	root
+)
