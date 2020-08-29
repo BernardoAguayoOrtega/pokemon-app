269306f7bb58e1837517bfb00114c4bfb8dec48e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 17:34:02 -0500

    add apollo client

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,25 @@
 //import react 
 import React from 'react'
+//import apllo client
+import ApolloClient from 'apollo-boost'
+//import APOLLO PROVIDER
+import { ApolloProvider } from '@apollo/react-hooks'
+//import pokemons container
+import { PokemonsContainer } from './containers/PokemonsContainer'
 
 const App = (props) => {
-	return <h1> App </h1>
+//create Apollo client
+const client = new ApolloClient({
+	uri: 'https://graphql-pokemon.now.sh'
+})
+
+	return (
+	<AppolloProvider client={client}>
+		<main>
+			<PokemonsContainer />
+		</main>
+	</ApolloProvider>
+)
 }
 
 //export app
